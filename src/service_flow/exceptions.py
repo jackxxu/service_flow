7b4fdb6d39f668b755de85ab2c40b178e12f4c64
Jack Xu <jackxxu@gmail.com>
2021-08-24 06:09:18 -0400

    update exceptions

--- src/service_flow/exceptions.py
+++ src/service_flow/exceptions.py
@@ -1,5 +1,9 @@
 class StopFlowException(Exception):
-    """ an exception to stop the execution of subsequent middlewares """
+    """ 
+    an exception to stop the execution of subsequent middlewares 
+    """
 
 class RetryException(Exception):
-    """ an exception to stop the execution of subsequent middlewares """
+    """ 
+    an exception to stop the execution of subsequent middlewares and signal for re-process the same input overall again
+    """
