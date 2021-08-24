f55153cf3f9309c94b70c19e43a6d78ba0520a9c
Jack Xu <jackxxu@gmail.com>
2021-08-24 06:07:57 -0400

    added RetryException

--- src/service_flow/exceptions.py
+++ src/service_flow/exceptions.py
@@ -1,2 +1,5 @@
 class StopFlowException(Exception):
     """ an exception to stop the execution of subsequent middlewares """
+
+class RetryException(Exception):
+    """ an exception to stop the execution of subsequent middlewares """
