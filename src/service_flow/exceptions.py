1a6f5bef5e57bc32b82bf5da56e2b67ea7a9adb4
Jack Xu <jackxxu@gmail.com>
2021-09-02 11:15:15 -0400

    added FatalException

--- src/service_flow/exceptions.py
+++ src/service_flow/exceptions.py
@@ -7,3 +7,8 @@ class RetryException(Exception):
     """ 
     an exception to stop the execution of subsequent middlewares and signal for re-process the same input overall again
     """
+
+class FatalException(Exception):
+    """ 
+    an exception to stop the execution of subsequent middlewares and for the processor to exit and start fresh
+    """
