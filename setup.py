11d87b6184f2c7eb2af57259591c2a52a39feff1
Jack Xu <jackxxu@gmail.com>
2022-10-16 20:34:03 -0400

    up to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="service_flow",
-    version="0.1.6",
+    version="0.2.0",
     author="Xiaosong Xu",
     author_email="jackxxu@gmail.com",
     description="service object flow orchestrator",
